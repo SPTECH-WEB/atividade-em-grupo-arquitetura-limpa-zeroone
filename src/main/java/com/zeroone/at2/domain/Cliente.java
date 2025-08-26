fde69c0fbb25fd7ce581e4882daec6c0f5841e05
GustavoLuzz <gustavo.barros@sptech.school>
2025-08-26 20:30:58 -0300

    feat: entity cliente

---
+++ src/main/java/com/zeroone/at2/domain/Cliente.java
@@ -0,0 +1,90 @@
+package com.zeroone.at2.domain;
+
+import java.util.UUID;
+
+public class Cliente {
+
+    private UUID id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private Double rendaMensal;
+    private Integer idade;
+    private String profissao;
+
+    public Cliente(UUID id, String nome, String email, String telefone, String cpf, Double rendaMensal, Integer idade, String profissao) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.rendaMensal = rendaMensal;
+        this.idade = idade;
+        this.profissao = profissao;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getRendaMensal() {
+        return rendaMensal;
+    }
+
+    public void setRendaMensal(Double rendaMensal) {
+        this.rendaMensal = rendaMensal;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+}
